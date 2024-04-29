178066fdcf0f6db39e05f225aa0fd181836b7d32
wasif903 <wasifmehmood903@gmail.com>
2024-04-30 04:20:36 +0500

    push

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ const app = express();
 app.use(express.json());
 
 
-
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+})
 
 app.use("/api", User)
 
 app.listen(5000, () => {
     console.log("APP Listening To")
 })
+
+
+export default app;
